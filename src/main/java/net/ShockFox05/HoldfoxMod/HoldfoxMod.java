734e2e4bfddea18af36ad4dc4fbbc3bbfe699643
ShockFox05 <dakotastodghill@gmail.com>
2025-03-20 05:56:14 -0400

    New Stuff

--- src/main/java/net/ShockFox05/HoldfoxMod/HoldfoxMod.java
+++ src/main/java/net/ShockFox05/HoldfoxMod/HoldfoxMod.java
@@ -1,6 +1,7 @@
 package net.ShockFox05.HoldfoxMod;
 
 import net.ShockFox05.HoldfoxMod.block.ModBlocks;
+import net.ShockFox05.HoldfoxMod.item.ModCreativeModeTabs;
 import net.ShockFox05.HoldfoxMod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -36,6 +37,8 @@ public class HoldfoxMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register((modEventBus));
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
---
+++ src/main/java/net/ShockFox05/HoldfoxMod/item/ModCreativeModeTabs.java
@@ -0,0 +1,40 @@
+package net.ShockFox05.HoldfoxMod.item;
+
+import net.ShockFox05.HoldfoxMod.HoldfoxMod;
+import net.ShockFox05.HoldfoxMod.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CRAETIVE_MODE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, HoldfoxMod.MOD_ID);
+
+    public static final Supplier<CreativeModeTab> HOLDFOX_ITEMS_TAB = CRAETIVE_MODE_TAB.register("holdfox_items_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.HOLDFOX.get()))
+                    .title(Component.translatable("creativetab.holdfoxmodsf.holdfox_items"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.HOLDFOX.get());
+                        output.accept(ModItems.DEATH.get());
+                    }).build());
+
+    public static final Supplier<CreativeModeTab> HOLDFOX_BLOCKS_TAB = CRAETIVE_MODE_TAB.register("holdfox_blocks_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.FOX_ORE.get()))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(HoldfoxMod.MOD_ID, "holdfox_items_tab"))
+                    .title(Component.translatable("creativetab.holdfoxmodsf.holdfox_blocks"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModBlocks.FOX_BLOCK.get());
+                        output.accept(ModBlocks.FOX_ORE.get());
+                    }).build());
+
+
+    public static void register(IEventBus eventBus) {
+        CRAETIVE_MODE_TAB.register(eventBus);
+    }
+}
