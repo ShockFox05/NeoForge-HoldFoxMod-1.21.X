b248750cfe90d0e36c35abea1b8b6be7b63f8f08
ShockFox05 <dakotastodghill@gmail.com>
2025-03-20 05:22:19 -0400

    New Stuff

--- src/main/java/net/ShockFox05/HoldfoxMod/HoldfoxMod.java
+++ src/main/java/net/ShockFox05/HoldfoxMod/HoldfoxMod.java
@@ -1,5 +1,6 @@
 package net.ShockFox05.HoldfoxMod;
 
+import net.ShockFox05.HoldfoxMod.block.ModBlocks;
 import net.ShockFox05.HoldfoxMod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -37,6 +38,7 @@ public class HoldfoxMod {
         NeoForge.EVENT_BUS.register(this);
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -53,6 +55,10 @@ public class HoldfoxMod {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(ModItems.HOLDFOX);
             event.accept(ModItems.DEATH);
+        }
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.FOX_BLOCK);
+            event.accept(ModBlocks.FOX_ORE);
         }
     }
 
