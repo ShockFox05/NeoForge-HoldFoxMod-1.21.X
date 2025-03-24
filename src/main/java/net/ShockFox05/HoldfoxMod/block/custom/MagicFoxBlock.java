12e664b179ad1b1751529890ca76014f0a3cee92
ShockFox05 <dakotastodghill@gmail.com>
2025-03-24 18:38:45 -0400

    ToolTips Galore

--- src/main/java/net/ShockFox05/HoldfoxMod/block/custom/MagicFoxBlock.java
+++ src/main/java/net/ShockFox05/HoldfoxMod/block/custom/MagicFoxBlock.java
@@ -1,19 +1,25 @@
 package net.ShockFox05.HoldfoxMod.block.custom;
 
 import net.ShockFox05.HoldfoxMod.item.ModItems;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+
+import java.util.List;
 
 public class MagicFoxBlock extends Block {
     public MagicFoxBlock(Properties properties) {
@@ -29,27 +35,36 @@ public class MagicFoxBlock extends Block {
 
     @Override
     public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
-        if(entity instanceof ItemEntity itemEntity) {
+        if (entity instanceof ItemEntity itemEntity) {
             if (itemEntity.getItem().getItem() == Items.GOLD_INGOT) {
                 // (ABOVE^) What item, turns into, (BELOW) What item
                 itemEntity.setItem(new ItemStack(Items.DIAMOND, itemEntity.getItem().getCount()));
             }
         }
 
-        if(entity instanceof ItemEntity itemEntity) {
+        if (entity instanceof ItemEntity itemEntity) {
             if (itemEntity.getItem().getItem() == Items.POPPY) {
                 // (ABOVE^) What item, turns into, (BELOW) What item
                 itemEntity.setItem(new ItemStack(Items.WITHER_ROSE, itemEntity.getItem().getCount()));
             }
         }
-        if(entity instanceof ItemEntity itemEntity) {
-            if(itemEntity.getItem().getItem() == ModItems.DEATH.get()) {
+        if (entity instanceof ItemEntity itemEntity) {
+            if (itemEntity.getItem().getItem() == ModItems.DEATH.get()) {
                 // (ABOVE^) What item, turns into, (BELOW) What item
                 itemEntity.setItem(new ItemStack(ModItems.HOLDFOX.get(), itemEntity.getItem().getCount()));
             }
         }
 
-
         super.stepOn(level, pos, state, entity);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        if (Screen.hasShiftDown()) {
+            tooltipComponents.add(Component.translatable("tooltip.holdfoxmodsf.magic_fox_block.shift_down"));
+        } else {
+            tooltipComponents.add(Component.translatable("tooltip.holdfoxmodsf.magic_fox_block.tooltip"));
+            super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+        }
     }
 }
--- src/main/java/net/ShockFox05/HoldfoxMod/item/ModItems.java
+++ src/main/java/net/ShockFox05/HoldfoxMod/item/ModItems.java
@@ -4,33 +4,85 @@ import net.ShockFox05.HoldfoxMod.HoldfoxMod;
 import net.ShockFox05.HoldfoxMod.item.custom.FoxChiselItem;
 import net.ShockFox05.HoldfoxMod.item.custom.FoxEssenceInABottleItem;
 import net.ShockFox05.HoldfoxMod.item.custom.FuelItem;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(HoldfoxMod.MOD_ID);
 
     public static final DeferredItem<Item> HOLDFOX = ITEMS.register("holdfox",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties()){
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+                    tooltipComponents.add(Component.translatable("tooltip.holdfoxmodsf.holdfox"));
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
     public static final DeferredItem<Item> DEATH = ITEMS.register("death",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties()){
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+                    tooltipComponents.add(Component.translatable("tooltip.holdfoxmodsf.death"));
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
     //basic items
 
     public static final DeferredItem<Item> FOX_CHISEL = ITEMS.register("fox_chisel",
             () -> new FoxChiselItem(new Item.Properties().durability(32)));
     public static final DeferredItem<Item> FIRE_ESSENCE = ITEMS.register("fire_essence",
-            () -> new FuelItem(new Item.Properties(),900));
+            () -> new FuelItem(new Item.Properties(),900){
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+                    tooltipComponents.add(Component.translatable("tooltip.holdfoxmodsf.fire_essence"));
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
     public static final DeferredItem<Item> UNSTABLE_FIRE = ITEMS.register("unstable_fire",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties()){
+        @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+                    tooltipComponents.add(Component.translatable("tooltip.holdfoxmodsf.unstable_fire"));
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
 
     //"advanced items"
 
     public static final DeferredItem<Item> EMERALD_APPLE = ITEMS.register("emerald_apple",
-            () -> new Item(new Item.Properties().food(ModFoodProperties.EMERALD_APPLE)));
+            () -> new Item(new Item.Properties().food(ModFoodProperties.EMERALD_APPLE)){
+        @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+            if (Screen.hasControlDown()) {
+                tooltipComponents.add(Component.translatable("tooltip.holdfoxmodsf.emerald_apple.ctrl_down"));
+            } else
+            {
+                tooltipComponents.add(Component.translatable("tooltip.holdfoxmodsf.emerald_apple"));
+            }
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
     public static final DeferredItem<Item> FOX_ESSENCE_IN_A_BOTTLE = ITEMS.register("fox_essence_in_a_bottle",
-            () -> new FoxEssenceInABottleItem(new Item.Properties().food(ModFoodProperties.FOX_ESSENCE_IN_A_BOTTLE)));
+            () -> new FoxEssenceInABottleItem(new Item.Properties().food(ModFoodProperties.FOX_ESSENCE_IN_A_BOTTLE)){
+        @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+            if (Screen.hasShiftDown()) {
+                tooltipComponents.add(Component.translatable("tooltip.holdfoxmodsf.fox_essence_in_a_bottle.shift_down"));
+            } else
+            {
+                tooltipComponents.add(Component.translatable("tooltip.holdfoxmodsf.fox_essence_in_a_bottle"));
+            }
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
     //Consumables
 
     public static void register(IEventBus eventBus) {
