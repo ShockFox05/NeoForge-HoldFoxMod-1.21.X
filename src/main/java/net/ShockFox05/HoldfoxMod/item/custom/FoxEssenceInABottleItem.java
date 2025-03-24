c2e898c5a3f4b28fc40257bd0e119cc3f96ee580
ShockFox05 <dakotastodghill@gmail.com>
2025-03-24 16:35:12 -0400

    Fuel Source update

--- src/main/java/net/ShockFox05/HoldfoxMod/item/custom/FoxEssenceInABottleItem.java
+++ src/main/java/net/ShockFox05/HoldfoxMod/item/custom/FoxEssenceInABottleItem.java
@@ -1,34 +1,18 @@
 package net.ShockFox05.HoldfoxMod.item.custom;
 
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.UseAnim;
-import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 
-
-public class FoxEssenceInABottleItem extends Item{
-
-    public FoxEssenceInABottleItem(Item.Properties properties) {
+public class FoxEssenceInABottleItem extends Item
+    {
+        public FoxEssenceInABottleItem(Item.Properties properties) {
         super(properties);
-    }
+        }
 
-    @Override
-    public InteractionResult useOn(UseOnContext context) {
-        Level level = context.getLevel();
-        Block clickedBlock = level.getBlockState(context.getClickedPos()).getBlock();
-                level.playSound(null,context.getClickedPos(), SoundEvents.GRINDSTONE_USE, SoundSource.BLOCKS);
-
-
-
-        return InteractionResult.SUCCESS;
-    }
-    @Override
-    public UseAnim getUseAnimation(ItemStack stack) {
+        @Override
+        public UseAnim getUseAnimation(ItemStack stack) {
         return UseAnim.DRINK;
+        }
+
     }
-}
