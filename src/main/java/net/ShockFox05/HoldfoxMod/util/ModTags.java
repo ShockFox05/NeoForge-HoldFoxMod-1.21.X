f55d745371152abae6e0215bb1a509c09f679d7b
ShockFox05 <dakotastodghill@gmail.com>
2025-03-26 02:24:02 -0400

    ToolTips Galore

--- src/main/java/net/ShockFox05/HoldfoxMod/util/ModTags.java
+++ src/main/java/net/ShockFox05/HoldfoxMod/util/ModTags.java
@@ -11,7 +11,7 @@ import net.minecraft.world.level.block.Block;
 public class ModTags {
     public static class Blocks {
 
-        public static final TagKey<Block> HOLDFOXMOD_BLOCKS = createTag("holdfoxmodsf_Blocks");
+        public static final TagKey<Block> HOLDFOXMOD_BLOCKS = createTag("holdfoxmodsf_blocks");
 
         private static TagKey<Block> createTag(String name) {
             return BlockTags.create(ResourceLocation.fromNamespaceAndPath(HoldfoxMod.MOD_ID, name));
