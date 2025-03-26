34ed9491ed35da4eb17b7e2eb986ce734091e627
ShockFox05 <dakotastodghill@gmail.com>
2025-03-26 02:30:40 -0400

    Added Tags

--- src/main/java/net/ShockFox05/HoldfoxMod/util/ModTags.java
+++ src/main/java/net/ShockFox05/HoldfoxMod/util/ModTags.java
@@ -18,7 +18,6 @@ public class ModTags {
         }
     }
     public static class Items {
-        
         public static final TagKey<Item> TRANSFORMABLE_ITEMS = createTag("transformable_items");
         public static final TagKey<Item> HOLDFOXMOD_ITEMS = createTag("holdfoxmodsf_items");
 
